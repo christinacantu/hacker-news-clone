caab11da6616d151b6b2796b1809caac7f2f9858
Christina Cantu <christinaccantu@gmail.com>
2018-07-04 17:44:27 -0500

    create bottom div, append bottom, append news item, add space between elements

--- main.js
+++ main.js
@@ -9,30 +9,45 @@ fetch(url)
     let newsList = document.createElement('ol');
     let container = document.querySelector('.container');
     container.appendChild(newsList);
+
     results.map(article => {
 
+      let articleUpvote = document.createElement('button');
+      articleUpvote.innerHTML = ' <a href="#"></a>';
+
       let articlePoints = document.createElement('a');
-      articlePoints.innerHTML = '<a href="#">50 Points by</a>';
+      articlePoints.innerHTML = ' <a href="#">50 Points by</a>';
 
-      // let articlePoints = document.createElement('button');
-      // articlePoints.innerHTML = '<a href="#">50 Points</a>';
+      let articleUser = document.createElement('a');
+      articleUser.innerHTML = ' <a href="#">Butterbutt</a>';
 
+      let articlePostTime = document.createElement('a');
+      articlePostTime.innerHTML = ' <a href="#">5 hours ago</a>';
 
+      let articleHide = document.createElement('a');
+      articleHide.innerHTML = ' <a href="#">Hide</a>';
 
-
-
+      let articleComments = document.createElement('a');
+      articleComments.innerHTML = ' <a href="#">10 comments</a>';
 
       let articleSource = document.createElement('a');
       let url = new URL(article.url);
-      // console.log(article.url);
-      // console.log(hostname);
-      articleSource.innerHTML = ' (' + url.hostname + ')';  //need to fix link
-      // console.log(url.hostname);
+      articleSource.innerHTML = ' (<a href="#">' + url.hostname + '</a>)';
 
       let newsItem = document.createElement('li');
       newsItem.innerHTML = '<a href="' + article.url + '">' + article.title + "</a>";
       newsItem.appendChild(articleSource);
-      newsItem.appendChild(articlePoints);
+
+
+      let bottom = document.createElement('div');
+      bottom.setAttribute('class', 'bottom');
+      bottom.appendChild(articlePoints);
+      bottom.appendChild(articleUser);
+      bottom.appendChild(articlePostTime);
+      bottom.appendChild(articleHide);
+      bottom.appendChild(articleComments);
+      newsItem.appendChild(bottom);
+      
       newsList.appendChild(newsItem);
     });
   })

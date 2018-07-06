55d63782039c60b0cd02885e6644b8836e346173
Christina Cantu <christinaccantu@gmail.com>
2018-07-05 21:21:15 -0500

    add link attributes, class attributes

--- main.js
+++ main.js
@@ -11,33 +11,45 @@ fetch(url)
     container.appendChild(newsList);
 
     results.map(article => {
-
       let articleUpvote = document.createElement('button');
-      articleUpvote.innerHTML = ' <a href="#"></a>';
+      articleUpvote.setAttribute('href', '#');
+      articleUpvote.setAttribute('class', 'upvote');
 
-      let articlePoints = document.createElement('a');
-      articlePoints.innerHTML = ' <a href="#">50 Points by</a>';
+      let articlePoints = document.createElement('p');
+      articlePoints.innerHTML = '50 Points by';
+      articlePoints.setAttribute('class', 'points');
+
 
       let articleUser = document.createElement('a');
-      articleUser.innerHTML = ' <a href="#">Butterbutt</a>';
+      articleUser.innerHTML = 'Butterbutt';
+      articleUser.setAttribute('href', '#');
+      articleUser.setAttribute('class', 'user');
 
       let articlePostTime = document.createElement('a');
-      articlePostTime.innerHTML = ' <a href="#">5 hours ago</a>';
+      articlePostTime.innerHTML = '5 hours ago';
+      articlePostTime.setAttribute('href', '#');
+      articlePostTime.setAttribute('class', 'post-time');
 
       let articleHide = document.createElement('a');
-      articleHide.innerHTML = ' <a href="#">Hide</a>';
+      articleHide.innerHTML = 'Hide';
+      articleHide.setAttribute('href', '#');
+      articleHide.setAttribute('class', 'hide');
 
       let articleComments = document.createElement('a');
-      articleComments.innerHTML = ' <a href="#">10 comments</a>';
+      articleComments.innerHTML = '10 comments';
+      articleComments.setAttribute('href', '#');
+      articleComments.setAttribute('class', 'comments');
 
       let articleSource = document.createElement('a');
       let url = new URL(article.url);
-      articleSource.innerHTML = ' (<a href="#">' + url.hostname + '</a>)';
+      articleSource.innerHTML = '(' + url.hostname + ')';
+      articleSource.setAttribute('class', 'url');
+      articleSource.setAttribute('href', '#');
 
       let newsItem = document.createElement('li');
       newsItem.innerHTML = '<a href="' + article.url + '">' + article.title + "</a>";
+      newsItem.insertBefore(articleUpvote, newsItem.childNodes[0]);
       newsItem.appendChild(articleSource);
-
 
       let bottom = document.createElement('div');
       bottom.setAttribute('class', 'bottom');
@@ -47,7 +59,7 @@ fetch(url)
       bottom.appendChild(articleHide);
       bottom.appendChild(articleComments);
       newsItem.appendChild(bottom);
-      
+
       newsList.appendChild(newsItem);
     });
   })
